a972bf635ea09bc45ed56a0ade23c94e083cc6ba
Bastien Jansen <bastien.jansen@gmx.com>
2019-12-17 21:10:30 +0100

    Fixed file icons not showing properly in IDEA 2019.3 (fixes #16).

--- src/main/java/org/antlr/jetbrains/st4plugin/Icons.java
+++ src/main/java/org/antlr/jetbrains/st4plugin/Icons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class Icons {
-	public static final Icon STG_FILE = IconLoader.getIcon("icons/st.png");
+	public static final Icon STG_FILE = IconLoader.getIcon("/icons/st.png");
 }
