7501b8bcff6d0fe0976ff35912a360bf09cd4a4a
Frédéric Hannes <freddy1990@gmail.com>
2020-05-19 11:56:49 +0200

    Updated STGroupHighlightingPassFactory class to a TextEditorHighlightingPassFactoryRegistrar introduced in 2019.2 to address issue #25

--- src/main/java/org/antlr/jetbrains/st4plugin/highlight/STGroupHighlightingPassFactory.java
+++ src/main/java/org/antlr/jetbrains/st4plugin/highlight/STGroupHighlightingPassFactory.java
@@ -2,26 +2,21 @@ package org.antlr.jetbrains.st4plugin.highlight;
 
 import com.intellij.codeHighlighting.TextEditorHighlightingPass;
 import com.intellij.codeHighlighting.TextEditorHighlightingPassFactory;
+import com.intellij.codeHighlighting.TextEditorHighlightingPassFactoryRegistrar;
 import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar;
-import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class STGroupHighlightingPassFactory
-	extends AbstractProjectComponent
-	implements TextEditorHighlightingPassFactory
+	implements TextEditorHighlightingPassFactory, TextEditorHighlightingPassFactoryRegistrar
 {
-	public STGroupHighlightingPassFactory(Project project,
-	                                      TextEditorHighlightingPassRegistrar registrar)
-	{
-		super(project);
-		registrar.registerTextEditorHighlightingPass(this, null, null, true, -1);
-	}
+	private Project project;
 
 	@Nullable
 	@Override
@@ -30,6 +25,13 @@ public class STGroupHighlightingPassFactory
 		Document doc = editor.getDocument();
 		VirtualFile vfile = FileDocumentManager.getInstance().getFile(doc);
 		if ( vfile==null || !vfile.getName().endsWith(".stg") ) return null;
-		return new STGroupHighlightingPass(myProject, editor);
+		return new STGroupHighlightingPass(project, editor);
 	}
+
+	@Override
+	public void registerHighlightingPassFactory(@NotNull TextEditorHighlightingPassRegistrar registrar, @NotNull Project project) {
+		this.project = project;
+		registrar.registerTextEditorHighlightingPass(this, null, null, true, -1);
+	}
+
 }
