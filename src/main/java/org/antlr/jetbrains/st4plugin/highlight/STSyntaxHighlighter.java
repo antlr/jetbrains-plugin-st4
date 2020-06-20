403b925fde8116002465ede212c19236dc15ebc2
Bastien Jansen <bastien.jansen@gmx.com>
2020-06-20 08:10:34 +0200

    Tweaks in the .st highlighter

--- src/main/java/org/antlr/jetbrains/st4plugin/highlight/STSyntaxHighlighter.java
+++ src/main/java/org/antlr/jetbrains/st4plugin/highlight/STSyntaxHighlighter.java
@@ -2,6 +2,7 @@ package org.antlr.jetbrains.st4plugin.highlight;
 
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+import static org.antlr.jetbrains.st4plugin.psi.STTokenTypes.getTokenElementType;
 
 public class STSyntaxHighlighter extends SyntaxHighlighterBase {
 
@@ -24,7 +26,7 @@ public class STSyntaxHighlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey ST_ID = STGroupSyntaxHighlighter.STGroup_TEMPLATE_NAME;
 
     private static final List<IElementType> KEYWORDS = Stream.of(
-            STLexer.IF, STLexer.ELSE, /* TODO STLexer.REGION_END, */ STLexer.TRUE,
+            STLexer.IF, STLexer.ELSE, STLexer.END, STLexer.TRUE,
             STLexer.FALSE, STLexer.ELSEIF, STLexer.ENDIF, STLexer.SUPER
     ).map(STTokenTypes::getTokenElementType).collect(Collectors.toList());
 
@@ -41,15 +43,17 @@ public class STSyntaxHighlighter extends SyntaxHighlighterBase {
 
         if (KEYWORDS.contains(tokenType)) {
             key = DefaultLanguageHighlighterColors.KEYWORD;
-        } else if (STTokenTypes.getTokenElementType(STLexer.ID).equals(tokenType)) {
+        } else if (getTokenElementType(STLexer.ID).equals(tokenType)) {
             key = ST_ID;
-        } else if (STTokenTypes.getTokenElementType(STLexer.STRING).equals(tokenType)
-                || STTokenTypes.getTokenElementType(STLexer.TEXT).equals(tokenType)) {
+        } else if (getTokenElementType(STLexer.STRING).equals(tokenType)
+                || getTokenElementType(STLexer.TEXT).equals(tokenType)) {
             key = STGroup_TEMPLATE_TEXT;
-        } else if (STTokenTypes.getTokenElementType(STLexer.TMPL_COMMENT).equals(tokenType)) {
+        } else if (getTokenElementType(STLexer.TMPL_COMMENT).equals(tokenType)) {
             key = DefaultLanguageHighlighterColors.BLOCK_COMMENT;
-//        } else if (STTokenTypes.getTokenElementType(STLexer.SUB_ERR_CHAR).equals(tokenType)) {
-//            key = HighlighterColors.BAD_CHARACTER;
+        } else if (getTokenElementType(STLexer.SUB_ERR_CHAR).equals(tokenType)) {
+            key = HighlighterColors.BAD_CHARACTER;
+        } else if (getTokenElementType(STLexer.ESCAPE).equals(tokenType)){
+            key = DefaultLanguageHighlighterColors.VALID_STRING_ESCAPE;
         } else {
             return STGroupSyntaxHighlighter.NO_ATTRIBUTES;
         }
